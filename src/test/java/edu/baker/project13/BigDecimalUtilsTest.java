f0cc6966ee2fb17b2825ab0c73843068788bfe2e
Mitchell Ogden <53495900+MitchOgden@users.noreply.github.com>
2023-10-14 17:00:48 -0400

    Add initial test files

---
+++ src/test/java/edu/baker/project13/BigDecimalUtilsTest.java
@@ -0,0 +1,119 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package edu.baker.project13;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author mitch
+ */
+public class BigDecimalUtilsTest {
+    
+    public BigDecimalUtilsTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testAdd() {
+    }
+
+    @Test
+    public void testSubtract() {
+    }
+
+    @Test
+    public void testMultiply() {
+    }
+
+    @Test
+    public void testDivide() {
+    }
+
+    @Test
+    public void testMin() {
+    }
+
+    @Test
+    public void testMax() {
+    }
+
+    @Test
+    public void testAbs() {
+    }
+
+    @Test
+    public void testPow() {
+    }
+
+    @Test
+    public void testSqrt() {
+    }
+
+    @Test
+    public void testSin() {
+    }
+
+    @Test
+    public void testCos() {
+    }
+
+    @Test
+    public void testTan() {
+    }
+
+    @Test
+    public void testAsin() {
+    }
+
+    @Test
+    public void testAcos() {
+    }
+
+    @Test
+    public void testAtan() {
+    }
+
+    @Test
+    public void testLog() {
+    }
+
+    @Test
+    public void testLog10() {
+    }
+
+    @Test
+    public void testFloor() {
+    }
+
+    @Test
+    public void testCeil() {
+    }
+
+    @Test
+    public void testRound() {
+    }
+    
+}

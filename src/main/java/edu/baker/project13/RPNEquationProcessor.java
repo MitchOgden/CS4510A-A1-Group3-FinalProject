f6f44e0785fad0555215f19fb2bdc53ab1d22c16
Mitchell Ogden <53495900+MitchOgden@users.noreply.github.com>
2023-10-15 09:48:56 -0400

    Add comments

--- src/main/java/edu/baker/project13/RPNEquationProcessor.java
+++ src/main/java/edu/baker/project13/RPNEquationProcessor.java
@@ -14,17 +14,37 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
 
+/**
+ * This class is responsible for processing RPN equations from an input source.
+ */
 public class RPNEquationProcessor {
+    // Instance of RPNCalculator to perform calculations
     private final RPNCalculator calc;
+    
+    // Reader for input
     private final BufferedReader input;
+    
+    // Map to store variables and their values
     private final Map<String, BigDecimal> variables = new HashMap<>();
-    private final MathContext mc = new MathContext(8);  // Precision can be adjusted if needed
-
+    
+    // MathContext for precision settings
+    private final MathContext mc = new MathContext(8);  
+    
+    /**
+     * Constructor that initializes the RPNCalculator and input reader.
+     * @param reader The reader for input.
+     */
     public RPNEquationProcessor(Reader reader) {
         this.calc = new RPNCalculator(mc);
         this.input = reader instanceof BufferedReader ? (BufferedReader) reader : new BufferedReader(reader);
     }
 
+    /**
+     * Processes the input equations and returns the final result.
+     * @param writer The writer for output.
+     * @return The final result as a BigDecimal.
+     * @throws IOException If an I/O error occurs.
+     */
     public BigDecimal processInput(Writer writer) throws IOException {
         BufferedWriter buffer = writer instanceof BufferedWriter ? (BufferedWriter) writer : new BufferedWriter(writer);
         String line;
@@ -38,20 +58,31 @@ public class RPNEquationProcessor {
         return calc.isEmpty() ? BigDecimal.ZERO : calc.peek();
     }
 
+    // Predefined variables that cannot be redefined
     private static final Set<String> predefinedVariables = new HashSet<>(List.of("x", "y", "e", "π", "pi"));
 
+    /**
+     * Processes a single command or equation.
+     * @param cmd The command to process.
+     * @return The result as a string.
+     */
     public String processCommand(String cmd) {
         if (cmd.startsWith("#")) return cmd;
         try {
+            // Handle variable assignments
             if (cmd.contains("=")) {
                 String[] parts = cmd.split("=");
                 String variable = parts[0].trim();
+                // Check if the variable is predefined and cannot be redefined
                 if (predefinedVariables.contains(variable)) {
                     return "Can't redefine " + variable;
                 }
+                // Assign the new value to the variable
                 variables.put(variable, new BigDecimal(parts[1]));
                 return cmd;
             }
+            
+            // Handle various commands and operations
             switch (cmd.toLowerCase()) {
                 case "add":
                 case "+":
@@ -155,12 +186,17 @@ public class RPNEquationProcessor {
                     break;
             }
         } catch (NumberFormatException ex) {
+            // Handle number format exceptions
             return "Illegal assignment: " + cmd;
         } catch (NoSuchElementException ex) {
+            // Handle empty stack exceptions
             return "Stack empty!";
         } catch (Exception ex) {
+            // Handle all other exceptions
             return ex.getClass().getSimpleName() + ": " + ex.getMessage();
         }
+        
+        // Return the current top value of the calculator stack
         return calc.peek().toString();
     }
 }

efe741e4fec11ff80e858886dfe660fcca6fb6f1
TylerColwell <86629266+TylerColwell@users.noreply.github.com>
2023-10-18 12:16:44 -0400

    Updated ceil, log10, factorial, asin, log and ipow methods
    Updated the unit tests for all of these methods as well.

---
+++ BigDecimalUtilsTest.java
@@ -0,0 +1,431 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package edu.baker.finalproject;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+/**
+ *
+ * 
+ */
+public class BigDecimalUtilsTest {
+    
+    MathContext mc = new MathContext(10);
+    
+    public BigDecimalUtilsTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testAdd() {
+    }
+
+    @Test
+    public void testSubtract() {
+    }
+
+    @Test
+    public void testMultiply() {
+    }
+
+    @Test
+    public void testDivide() {
+    }
+
+    @Test
+    public void testMin() {
+    }
+
+    @Test
+    public void testMax() {
+    }
+
+    @Test
+    public void testAbs() {
+    }
+
+    @Test
+    public void testPow() {
+        BigDecimal base = new BigDecimal("2", mc);
+        BigDecimal power = new BigDecimal("3", mc);
+        BigDecimal expected = new BigDecimal(Math.pow(base.doubleValue(), power.doubleValue()), mc);
+        BigDecimal result = BigDecimalUtils.pow(base, power, mc);
+        assertEquals(expected, result);
+    }
+    
+        @Test
+    public void testIpow() {
+        MathContext mc = new MathContext(10); // Customize the MathContext as needed
+        BigDecimal base = new BigDecimal("2", mc);
+        BigDecimal power = new BigDecimal("3", mc);
+        BigDecimal expected = new BigDecimal(Math.pow(base.doubleValue(), power.doubleValue()), mc);
+        BigDecimal result = BigDecimalUtils.ipow(base, power, mc); // Replace YourClass with the actual class name
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testSqrt() {
+    }
+
+    // Tests for iroot
+    @Test
+    void testIrootCubeRoot() {
+        BigDecimal base = new BigDecimal(27);
+        BigDecimal root = new BigDecimal(3);
+        BigDecimal result = BigDecimalUtils.iroot(base, root, mc);
+        assertEquals(new BigDecimal(3).setScale(mc.getPrecision(), mc.getRoundingMode()), result);
+    }
+
+    @Test
+    void testIrootSquareRoot() {
+        BigDecimal base = new BigDecimal(16);
+        BigDecimal root = new BigDecimal(2);
+        BigDecimal result = BigDecimalUtils.iroot(base, root, mc);
+        assertEquals(new BigDecimal(4).setScale(mc.getPrecision(), mc.getRoundingMode()), result);
+    }
+    
+    @Test
+    public void testSin() {
+        BigDecimal input = new BigDecimal("0.5", mc);
+        BigDecimal expected = new BigDecimal(Math.sin(input.doubleValue()), mc);
+        BigDecimal result = BigDecimalUtils.sin(input, mc);
+        assertEquals(expected, result);
+    }
+
+    /**
+     * Test of BigDecimal cos method, of class BigDecimalUtils
+     */
+    @Test 
+    public void testCos(){
+        BigDecimal radians = new BigDecimal("1.04719755119659774615421446109316762806572313312535");
+        MathContext var = new MathContext(15);
+        BigDecimal result = BigDecimalUtils.cos(radians, var);
+        
+        BigDecimal expected = new BigDecimal("0.500000000000000");
+        
+        assertEquals(expected, result);
+        
+        BigDecimal pi = BigDecimalUtils.pi(mc);
+        BigDecimal negativePi = pi.negate();
+        
+        BigDecimal result1 = BigDecimalUtils.cos(pi, mc);
+        BigDecimal result2 = BigDecimalUtils.cos(negativePi, mc);
+        
+        assertEquals(BigDecimal.ONE.negate(), result1, "Cosine of pi is -1");
+        assertEquals(BigDecimal.ONE.negate(), result2, "Cosine of negative pi is -1");
+    }
+
+    /**
+     * Test of BigDecimal tan method, of class BigDecimalUtils
+     */
+    @Test
+    public void testTan(){
+        //Test positive input
+        BigDecimal positiveInput = new BigDecimal(1);
+        BigDecimal positiveExpected = new BigDecimal(Math.tan(1), mc);
+        BigDecimal positiveResult = BigDecimalUtils.tan(positiveInput, mc);
+        assertEquals(positiveExpected, positiveResult);
+        
+        //Test negative input 
+        BigDecimal negativeInput = new BigDecimal(-1);
+        BigDecimal negativeExpected = new BigDecimal(Math.tan(-1), mc);
+        BigDecimal negativeResult = BigDecimalUtils.tan(negativeInput, mc);
+        assertEquals(negativeExpected, negativeResult);
+        
+        //Test input of zero
+        BigDecimal zeroInput = new BigDecimal(0);
+        BigDecimal zeroExpected = new BigDecimal(Math.tan(0), mc);
+        BigDecimal zeroResult = BigDecimalUtils.tan(zeroInput, mc);
+        assertEquals(zeroExpected, zeroResult);       
+    }
+
+      @Test
+    public void testAsin() {
+        MathContext mc = new MathContext(15);
+
+        // Test positive input
+        BigDecimal positiveInput = new BigDecimal("0.5");
+        BigDecimal positiveExpected = new BigDecimal(Math.asin(0.5), mc);
+        BigDecimal positiveResult = BigDecimalUtils.asin(positiveInput, mc);
+        assertEquals(positiveExpected, positiveResult, "Arcsine of 0.5 should match the expected result");
+
+        // Test negative input
+        BigDecimal negativeInput = new BigDecimal("-0.7");
+        BigDecimal negativeExpected = new BigDecimal(Math.asin(-0.7), mc);
+        BigDecimal negativeResult = BigDecimalUtils.asin(negativeInput, mc);
+        assertEquals(negativeExpected, negativeResult, "Arcsine of -0.7 should match the expected result");
+
+        // Test input of zero
+        BigDecimal zeroInput = new BigDecimal("0");
+        BigDecimal zeroExpected = new BigDecimal(Math.asin(0), mc);
+        BigDecimal zeroResult = BigDecimalUtils.asin(zeroInput, mc);
+        assertEquals(zeroExpected, zeroResult, "Arcsine of 0 should match the expected result");
+    }
+
+    @Test
+    public void testAcos() {
+    }
+
+    @Test
+    public void testAtan() {
+    }
+    
+    @Test
+    public void testLog() {
+        MathContext mc = new MathContext(15);
+
+        // Test a valid logarithm calculation
+        BigDecimal input = new BigDecimal("10.0");
+        BigDecimal result = BigDecimalUtils.log(input, mc);
+        BigDecimal expected = new BigDecimal("2.30258509299405");
+        assertEquals(expected, result, "Logarithm of 10.0 should be approximately 2.302585092994046");
+
+        // Test the logarithm of 1.0 should be 0.0
+       BigDecimal input1 = new BigDecimal("1.0");
+        BigDecimal result1 = BigDecimalUtils.log(input1, mc);
+        BigDecimal expected1 = new BigDecimal("0");
+        assertEquals(expected1, result1, "Logarithm of 1.0 should be 0.0");
+
+        // Test an invalid input (non-positive value)
+       final BigDecimal input2 = new BigDecimal("0.0");
+        assertThrows(ArithmeticException.class, () -> BigDecimalUtils.log(input2, mc), "Logarithm of non-positive value should throw an exception");
+
+       final BigDecimal input3 = new BigDecimal("-5.0");
+        assertThrows(ArithmeticException.class, () -> BigDecimalUtils.log(input3, mc), "Logarithm of non-positive value should throw an exception");
+    }
+@Test
+public void testLog10() {
+    MathContext mc = new MathContext(15);
+
+    // Test a positive input
+    BigDecimal positiveInput = new BigDecimal("100.0");
+    BigDecimal positiveExpected = new BigDecimal("2"); // Updated expected value
+    BigDecimal positiveResult = BigDecimalUtils.log10(positiveInput, mc);
+    assertEquals(0, positiveExpected.compareTo(positiveResult), "Log10 of 100.0 should match the expected result");
+
+    // Test a positive input that's not a power of 10
+    BigDecimal nonPowerOf10Input = new BigDecimal("30.0");
+    BigDecimal nonPowerOf10Expected = new BigDecimal("1.47712125471966", mc); // Updated expected value
+    BigDecimal nonPowerOf10Result = BigDecimalUtils.log10(nonPowerOf10Input, mc);
+    assertEquals(0, nonPowerOf10Expected.compareTo(nonPowerOf10Result), "Log10 of 30.0 should match the expected result");
+
+    // Test an invalid input (non-positive value)
+    BigDecimal zeroInput = new BigDecimal("0.0");
+    assertThrows(ArithmeticException.class, () -> BigDecimalUtils.log10(zeroInput, mc), "Log10 of non-positive value should throw an exception");
+
+    BigDecimal negativeInput = new BigDecimal("-5.0");
+    assertThrows(ArithmeticException.class, () -> BigDecimalUtils.log10(negativeInput, mc), "Log10 of non-positive value should throw an exception");
+}
+
+    // Various tests for BigDecimalUtil.floor works as intended
+    
+    @Test
+    public void testFloorPositiveValue() {
+        BigDecimal input = new BigDecimal("10.75");
+        BigDecimal result = BigDecimalUtils.floor(input);
+        assertEquals(0, result.compareTo(new BigDecimal("10")));
+    }
+
+    @Test
+    public void testFloorNegativeValue() {
+        BigDecimal input = new BigDecimal("-10.75");
+        BigDecimal result = BigDecimalUtils.floor(input);
+        assertEquals(0, result.compareTo(new BigDecimal("-11")));
+    }
+
+    @Test
+    public void testFloorZero() {
+        BigDecimal input = BigDecimal.ZERO;
+        BigDecimal result = BigDecimalUtils.floor(input);
+        assertEquals(0, result.compareTo(BigDecimal.ZERO));
+    }
+
+    @Test
+    public void testFloorIntegerValue() {
+        BigDecimal input = new BigDecimal("11");
+        BigDecimal result = BigDecimalUtils.floor(input);
+        assertEquals(0, result.compareTo(new BigDecimal("11")));
+    }
+
+    @Test
+    public void testCeil() {
+        BigDecimal test1 = new BigDecimal("3.14");
+        BigDecimal result1 = BigDecimalUtils.ceil(test1, mc);
+        System.out.println(result1.equals(new BigDecimal("4"))); // Output: true
+
+        BigDecimal test2 = new BigDecimal("-2.718");
+        BigDecimal result2 = BigDecimalUtils.ceil(test2, mc);
+        System.out.println(result2.equals(new BigDecimal("-2"))); // Output: true
+
+        BigDecimal test3 = new BigDecimal("0");
+        BigDecimal result3 = BigDecimalUtils.ceil(test3, mc);
+        System.out.println(result3.equals(new BigDecimal("0"))); // Output: true
+    }
+
+    @Test
+    public void testE() {
+        BigDecimal expected = new BigDecimal(Math.E, mc);
+        BigDecimal result = BigDecimalUtils.e(mc);
+        assertEquals(expected, result);
+    }
+    
+    @Test
+    public void testRound() {
+        BigDecimal x = new BigDecimal("3.14159", mc);
+        BigDecimal expected = new BigDecimal(Math.round(x.doubleValue()), mc);
+        BigDecimal result = BigDecimalUtils.round(x, mc);
+        assertEquals(expected, result);
+    }
+
+    /**
+     * Test of BigDecimal trunc method, of class BigDecimalUtils
+     */
+    @Test 
+    public void testTrunc(){
+        //Positive value test 
+        BigDecimal positiveInput = new BigDecimal("12.3456789");
+        BigDecimal positiveResult = BigDecimalUtils.trunc(positiveInput, mc);
+        BigDecimal positiveExpected = new BigDecimal("12");
+        assertEquals(positiveExpected, positiveResult);
+        
+        //Negative value test
+        BigDecimal negativeInput = new BigDecimal("-12.3456789");
+        BigDecimal negativeResult = BigDecimalUtils.trunc(negativeInput, mc);
+        BigDecimal negativeExpected = new BigDecimal("-12");
+        assertEquals(negativeExpected, negativeResult);
+        
+        //Trunc with zero
+        BigDecimal zeroInput = BigDecimal.ZERO;
+        BigDecimal zeroResult = BigDecimalUtils.trunc(zeroInput, mc);
+        BigDecimal zeroExpected = BigDecimal.ZERO;
+        assertEquals(zeroExpected,zeroResult);
+    }
+
+    /**
+     * Test of BigDecimal exp method, of class BigDecimalUtils
+     */
+    @Test
+    public void testExp(){
+        //Test e^2
+        BigDecimal positiveInput = new BigDecimal(2);
+        BigDecimal positiveExpected = new BigDecimal(Math.exp(2), mc);
+        BigDecimal positiveResult = BigDecimalUtils.exp(positiveInput, mc);
+        assertEquals(positiveExpected, positiveResult);
+        
+        //Test e^-2
+        BigDecimal negativeInput = new BigDecimal(-2);
+        BigDecimal negativeExpected = new BigDecimal(Math.exp(-2), mc);
+        BigDecimal negativeResult = BigDecimalUtils.exp(negativeInput, mc);
+        assertEquals(negativeExpected,negativeResult);
+        
+        //Test e^0
+        BigDecimal zeroInput = new BigDecimal(0);
+        BigDecimal zeroExpected = new BigDecimal(Math.exp(0), mc);
+        BigDecimal zeroResult = BigDecimalUtils.exp(zeroInput, mc);
+        assertEquals(zeroExpected,zeroResult);
+        
+        //Test e^100
+        BigDecimal largeInput = new BigDecimal(100);
+        BigDecimal largeExpected = new BigDecimal(Math.exp(100), mc);
+        BigDecimal largeResult = BigDecimalUtils.exp(largeInput, mc);
+        assertEquals(largeExpected,largeResult);
+    }
+
+    /**
+     * Test of BigDecimal pi method, of class BigDecimalUtils
+     */
+    @Test
+    public void testPi(){
+        BigDecimal expected = new BigDecimal(Math.PI, mc);
+        BigDecimal result = BigDecimalUtils.pi(mc);
+        assertEquals(expected, result);
+    }
+
+    /** 
+     * Test of BigDecimal atan2 method, of class BigDecimalUtils
+     */
+    @Test
+    public void testAtan2(){
+        //Test atan2(1, 1)
+        BigDecimal y = new BigDecimal("1");
+        BigDecimal x = new BigDecimal("1");
+        BigDecimal result = BigDecimalUtils.atan2(y, x, mc);
+        BigDecimal expected = new BigDecimal(Math.PI / 4, mc);
+        assertEquals(expected, result);
+        
+        //Test atan2 (1, -1)
+        y = new BigDecimal("1");
+        x = new BigDecimal("-1");
+        result = BigDecimalUtils.atan2(y, x, mc);
+        expected = new BigDecimal(3 * Math.PI / 4, mc);
+        assertEquals(expected, result);
+        
+        //Test atan2 (0, 1)
+        y = new BigDecimal("0");
+        x = new BigDecimal("1");
+        result = BigDecimalUtils.atan2(y, x, mc);
+        expected = BigDecimal.ZERO;
+        assertEquals(expected, result);
+        
+        //Test atan2 (0, 0)
+        y = BigDecimal.ZERO;
+        x = BigDecimal.ZERO;
+        result = BigDecimalUtils.atan2(y, x, mc);
+        expected = BigDecimal.ZERO;
+        assertEquals(expected, result);     
+    }
+    
+    @Test
+    public void testFactorial() {
+        // Test with a positive integer
+        BigDecimal input = new BigDecimal(5);
+        MathContext mc = MathContext.DECIMAL32;
+        BigDecimal expected = new BigDecimal(120);
+        assertEquals(expected, BigDecimalUtils.factorial(input, mc));
+
+        // Test with a decimal number
+        input = new BigDecimal(4.5);
+        expected = new BigDecimal(24);
+        assertEquals(expected, BigDecimalUtils.factorial(input, mc));
+
+        // Test with zero
+        input = new BigDecimal(0);
+        expected = new BigDecimal(1);
+        assertEquals(expected, BigDecimalUtils.factorial(input, mc));
+
+        // Test with a negative number (expecting an exception)
+        input = new BigDecimal(-3);
+        boolean exceptionThrown = false;
+        try {
+            BigDecimalUtils.factorial(input, mc);
+        } catch (IllegalArgumentException e) {
+            exceptionThrown = true;
+        }
+        assertTrue(exceptionThrown);
+    }
+}
+    
---
+++ RPNCalculator.java
@@ -0,0 +1,298 @@
+package edu.baker.finalproject;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * RPNCalculator class for performing Reverse Polish Notation calculations.
+ * Utilizes BigDecimal for arbitrary-precision arithmetic.
+ */
+public class RPNCalculator {
+    
+    // Stack to hold BigDecimal numbers for calculations
+    private final Deque<BigDecimal> stack = new ArrayDeque<>();
+    
+    // MathContext object for controlling precision and rounding in BigDecimal operations
+    private final MathContext mathContext;
+
+    /**
+     * Constructor to initialize the RPNCalculator with a MathContext.
+     * @param mathContext MathContext for BigDecimal operations.
+     */
+    public RPNCalculator(MathContext mathContext) {
+        this.mathContext = mathContext;
+    }
+
+    /**
+     * Checks if the stack is empty.
+     * @return boolean indicating if the stack is empty.
+     */
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    /**
+     * Peeks at the top element of the stack without removing it.
+     * @return Top element of the stack.
+     */
+    public BigDecimal peek() {
+        return stack.peek();
+    }
+
+    /**
+     * Swaps the top two elements of the stack.
+     */
+    public BigDecimal peekY() {
+        swap();
+        BigDecimal y = stack.peek();
+        swap();
+        return y;
+    }
+
+    /**
+     * Swaps the top two elements of the stack.
+     */
+    public void swap() {
+        BigDecimal x = stack.pop(); 
+        BigDecimal y = stack.pop();
+        value(x);
+        value(y);
+    }
+
+    /**
+     * Removes the top element from the stack.
+     */
+    public void rollDown() {
+        stack.pop();
+    }
+
+    /**
+     * Clears all elements from the stack.
+     */
+    public void clear() {
+        stack.clear();
+    }
+
+    /**
+     * Pushes a BigDecimal value onto the stack.
+     * @param v BigDecimal value to be pushed.
+     */
+    public void value(BigDecimal v) {
+        stack.push(v);
+    }
+
+    /**
+     * Adds the top two elements on the stack and pushes the result back onto the stack.
+     */
+    public void add() {
+        value(BigDecimalUtils.add(stack.pop(), stack.pop(), mathContext));
+    }
+
+    /**
+     * Subtracts the top element from the second top element on the stack and pushes the result back onto the stack.
+     */
+    public void sub() {
+        BigDecimal x = stack.pop();
+        BigDecimal y = stack.pop();
+        value(BigDecimalUtils.subtract(x, y, mathContext));
+    }
+
+    /**
+     * Multiplies the top two elements on the stack and pushes the result back onto the stack.
+     */
+    public void mult() {
+        value(BigDecimalUtils.multiply(stack.pop(), stack.pop(), mathContext));
+    }
+
+    /**
+     * Divides the second top element by the top element on the stack and pushes the result back onto the stack.
+     */
+    public void div() {
+        BigDecimal x = stack.pop();
+        BigDecimal y = stack.pop();
+        value(BigDecimalUtils.divide(x, y, mathContext));
+    }
+
+    /**
+     * Finds the minimum of the top two elements on the stack and pushes the result back onto the stack.
+     */
+    public void min() {
+        BigDecimal x = stack.pop();
+        BigDecimal y = stack.pop();
+        value(BigDecimalUtils.min(x, y));
+    }
+
+    /**
+     * Finds the maximum of the top two elements on the stack and pushes the result back onto the stack.
+     */
+    public void max() {
+        BigDecimal x = stack.pop();
+        BigDecimal y = stack.pop();
+        value(BigDecimalUtils.max(x,y));
+    }
+
+    /**
+     * Finds the absolute value of the top element on the stack and pushes the result back onto the stack.
+     */
+    public void abs() {
+        value(stack.pop().abs(mathContext));
+    }
+
+    /**
+     * Negates the top element on the stack and pushes the result back onto the stack.
+     */
+    public void neg() {
+        value(stack.pop().negate(mathContext));
+    }
+
+    /**
+     * Rounds the top element on the stack down to the nearest integer and pushes the result back onto the stack.
+     */
+    public void floor() {
+        value(BigDecimalUtils.floor(stack.pop()));
+    }
+
+    /**
+     * Rounds the top element on the stack up to the nearest integer and pushes the result back onto the stack.
+     */
+    public void ceil() {
+        value(BigDecimalUtils.ceil(stack.pop(),mathContext ));
+    }
+
+    /**
+     * Rounds the top element on the stack to the nearest integer and pushes the result back onto the stack.
+     */
+    public void round() {
+        value(stack.pop().round(mathContext));
+    }
+
+    /**
+     * Raises the second top element on the stack to the power of the top element and pushes the result back onto the stack.
+     */
+    public void ipow() {
+        BigDecimal x = stack.pop();
+        BigDecimal y = stack.pop();
+        value(BigDecimalUtils.pow(y, x, mathContext));
+    }
+
+    /**
+     * Raises the second top element on the stack to the power of the top element and pushes the result back onto the stack.
+     */
+    public void pow() {
+        BigDecimal x = stack.pop();
+        BigDecimal y = stack.pop();
+        value(BigDecimalUtils.pow(y, x, mathContext));
+    }
+    
+    
+/*
+    public void root() {
+        BigDecimal x = stack.pop();
+        BigDecimal y = stack.pop();
+        value(BigDecimalUtils.pow(y, BigDecimal.ONE.divide(x, mathContext), mathContext));
+    }
+*/
+    
+    /**
+     * Calculates the root of the second top element on the stack using the top element as the root value and pushes the result back onto the stack.
+     */
+    public void root() {
+        BigDecimal x = stack.pop();
+        BigDecimal y = stack.pop();
+        // WARNING: This approach might lose precision
+        value(BigDecimalUtils.iroot(y, x, mathContext));
+    }
+
+    /**
+     * Calculates the exponential function of the top element on the stack and pushes the result back onto the stack.
+     */
+    public void exp() {
+        value(BigDecimalUtils.exp10(stack.pop(), mathContext));
+    }
+
+    /**
+     * Calculates the natural logarithm of the top element on the stack and pushes the result back onto the stack.
+     */
+    public void log() {
+        value(BigDecimalUtils.log(stack.pop(), mathContext));
+    }
+
+    /**
+     * Calculates the base-10 logarithm of the top element on the stack and pushes the result back onto the stack.
+     */
+    public void log10() {
+        value(BigDecimalUtils.log10(stack.pop(), mathContext));
+    }
+
+    /**
+     * Calculates the cosine of the top element on the stack and pushes the result back onto the stack.
+     */
+    public void cos() {
+        value(BigDecimalUtils.cos(stack.pop(), mathContext));
+    }
+
+    /**
+     * Calculates the sine of the top element on the stack and pushes the result back onto the stack.
+     */
+    public void sin() {
+        value(BigDecimalUtils.sin(stack.pop(), mathContext));
+    }
+
+    /**
+     * Calculates the tangent of the top element on the stack and pushes the result back onto the stack.
+     */
+    public void tan() {
+        value(BigDecimalUtils.tan(stack.pop(), mathContext));
+    }
+
+    /**
+     * Calculates the arccosine of the top element on the stack and pushes the result back onto the stack.
+     */
+    public void acos() {
+        value(BigDecimalUtils.acos(stack.pop(), mathContext));
+    }
+
+    /**
+     * Calculates the arcsine of the top element on the stack and pushes the result back onto the stack.
+     */
+    public void asin() {
+        value(BigDecimalUtils.asin(stack.pop(), mathContext));
+    }
+
+    /**
+     * Calculates the arctangent of the top element on the stack and pushes the result back onto the stack.
+     */
+    public void atan() {
+        value(BigDecimalUtils.atan(stack.pop(), mathContext));
+    }
+
+    /**
+     * Pushes the constant e onto the stack.
+     */
+    public void e() {
+        value(BigDecimalUtils.e(mathContext));
+    }
+
+    /**
+     * Pushes the constant π (Pi) onto the stack.
+     */
+    public void π() {
+        value(BigDecimalUtils.pi(mathContext));
+    }
+
+    /**
+     * Placeholder for future implementation related to variable x.
+     */
+    void x() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    /**
+     * Placeholder for future implementation related to variable y.
+     */
+    void y() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
